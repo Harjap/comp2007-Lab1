f5a4c19e964f48b8e01c712157cc081a40b43dbe
Harjap Singh <singhharjap200@gmail.com>
2018-02-13 20:19:58 -0500

    lab 1

---
+++ comp2007labs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp2007labs.Startup))]
+namespace comp2007labs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
